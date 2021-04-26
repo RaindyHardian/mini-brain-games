3ac6bc4eeb6f5f14e3538b323df26d91654da131
Raindy Hardiansyah <raindyhardian@gmail.com>
2021-04-26 10:38:34 +0700

    add navbar

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./page.css";
 
 const Page = ({ children, pageType }) => {
+  if (pageType !== "home") {
+    return (
+      <div>
+        <nav className="page__nav">
+          <Link to="/" className="page__nav_back">
+            {"<"} Back to Home
+          </Link>
+        </nav>
+        <main>{children}</main>
+      </div>
+    );
+  }
+
   return (
     <div>
       <main>{children}</main>
